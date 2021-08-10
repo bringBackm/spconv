d6c058beeb7a3d66a46a582e2ed2d887385b2b09
Wang Shengfan <shengfan.wang@uisee.com>
2021-08-10 20:24:27 +0800

    first commit

---
+++ build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/shengfan/pre_label_tool/depends/spconv/src/spconv/indice.cu" "src/spconv/CMakeFiles/spconv.dir/indice.cu.o" "gcc" "src/spconv/CMakeFiles/spconv.dir/indice.cu.o.d"
+  "/home/user/shengfan/pre_label_tool/depends/spconv/src/spconv/maxpool.cu" "src/spconv/CMakeFiles/spconv.dir/maxpool.cu.o" "gcc" "src/spconv/CMakeFiles/spconv.dir/maxpool.cu.o.d"
+  "/home/user/shengfan/pre_label_tool/depends/spconv/src/spconv/reordering.cu" "src/spconv/CMakeFiles/spconv.dir/reordering.cu.o" "gcc" "src/spconv/CMakeFiles/spconv.dir/reordering.cu.o.d"
+  "/home/user/shengfan/pre_label_tool/depends/spconv/src/spconv/all.cc" "src/spconv/CMakeFiles/spconv.dir/all.cc.o" "gcc" "src/spconv/CMakeFiles/spconv.dir/all.cc.o.d"
+  "/home/user/shengfan/pre_label_tool/depends/spconv/src/spconv/indice.cc" "src/spconv/CMakeFiles/spconv.dir/indice.cc.o" "gcc" "src/spconv/CMakeFiles/spconv.dir/indice.cc.o.d"
+  "/home/user/shengfan/pre_label_tool/depends/spconv/src/spconv/maxpool.cc" "src/spconv/CMakeFiles/spconv.dir/maxpool.cc.o" "gcc" "src/spconv/CMakeFiles/spconv.dir/maxpool.cc.o.d"
+  "/home/user/shengfan/pre_label_tool/depends/spconv/src/spconv/reordering.cc" "src/spconv/CMakeFiles/spconv.dir/reordering.cc.o" "gcc" "src/spconv/CMakeFiles/spconv.dir/reordering.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "../../../lib.linux-x86_64-3.6/spconv/libspconv.pdb"
+  "../../../lib.linux-x86_64-3.6/spconv/libspconv.so"
+  "CMakeFiles/spconv.dir/all.cc.o"
+  "CMakeFiles/spconv.dir/all.cc.o.d"
+  "CMakeFiles/spconv.dir/cmake_device_link.o"
+  "CMakeFiles/spconv.dir/indice.cc.o"
+  "CMakeFiles/spconv.dir/indice.cc.o.d"
+  "CMakeFiles/spconv.dir/indice.cu.o"
+  "CMakeFiles/spconv.dir/indice.cu.o.d"
+  "CMakeFiles/spconv.dir/maxpool.cc.o"
+  "CMakeFiles/spconv.dir/maxpool.cc.o.d"
+  "CMakeFiles/spconv.dir/maxpool.cu.o"
+  "CMakeFiles/spconv.dir/maxpool.cu.o.d"
+  "CMakeFiles/spconv.dir/reordering.cc.o"
+  "CMakeFiles/spconv.dir/reordering.cc.o.d"
+  "CMakeFiles/spconv.dir/reordering.cu.o"
+  "CMakeFiles/spconv.dir/reordering.cu.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/spconv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
